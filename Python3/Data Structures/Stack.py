3546ccad199a0a2b3774cbb9a0f26afa6179c68b
randhana <pulathisi1921@gmail.com>
2022-11-25 13:18:56 +0530

    stack implimentation

---
+++ Python3/Data Structures/Stack.py
@@ -0,0 +1,22 @@
+#stack
+#real world application: Undo function
+my_stack = []
+
+#push data
+def push(stack, value):
+    stack.append(value)
+
+#pop data
+#.pop built  in function in py, 
+# no need to track pointer
+    
+def pop(stack):
+    return stack.pop()  
+
+
+push(my_stack,10)
+push(my_stack,20)
+print("new my stack: ",my_stack)
+pop(my_stack)
+print("After pop my stack: ",my_stack)
+
